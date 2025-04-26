f593e3595c514f4d53f7eda0d82816d541c4a42e
pardeep <your-email@example.com>
2025-04-26 12:21:32 +0530

    update

--- js/1.js
+++ js/1.js
@@ -1,2 +1,24 @@
-let x = 6;
-console.log(x);
+// let x = 6;
+// console.log(x)
+
+// let x = 6;
+// console.log(x)
+// let x = 12;
+// console.log(x);
+
+// let x = 6;
+// console.log(x)
+// x = 33;
+// console.log(x);
+
+// console.log(x)
+// let x = 6;
+
+
+// const x = 33;
+// console.log(x);
+// x = 34;
+// console.log(x);
+
+
+
